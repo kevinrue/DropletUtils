c90c1868cb53f0718a5aeeb20d578ab93719931f
Kevin Rue <kevinrue@users.noreply.github.com>
2026-08-19 20:49:56 +0100

    add basic code to biocjobs script

--- inst/biocjobs/scripts/read-10x-counts.R
+++ inst/biocjobs/scripts/read-10x-counts.R
@@ -7,16 +7,52 @@
 params <- BiocJobs::jobParams("DropletUtils", "read-10x-counts")
 
 suppressPackageStartupMessages(library(DropletUtils))
+suppressPackageStartupMessages(library(LoomExperiment))
 
 ## ---- inputs -------------------------------------------------------------
 
+tmpdir <- tempdir()
 
+### test data
 
-## ---- model --------------------------------------------------------------
+library(Matrix)
+my.counts <- abs(rsparsematrix(100, 10, 0.2) * 10)
+cell.ids <- paste0("BARCODE-", seq_len(ncol(my.counts)))
 
+ngenes <- nrow(my.counts)
+gene.ids <- paste0("ENSG0000", seq_len(ngenes))
+gene.symb <- paste0("GENE", seq_len(ngenes))
 
+test_matrix_dir <- file.path(tmpdir, "test_matrix")
+
+DropletUtils::write10xCounts(path = test_matrix_dir, x = my.counts, gene.id=gene.ids, 
+    gene.symbol=gene.symb, barcodes=cell.ids, type = "mtx")
+
+### process test inputs
+
+params <- list(
+    mtx_file = file.path(tmpdir, "test_matrix", "matrix.mtx"),
+    barcodes_file = file.path(tmpdir, "test_matrix", "barcodes.tsv"),
+    genes_file = file.path(tmpdir, "test_matrix", "genes.tsv"),
+    sample_name = "sample_name",
+    outfile = file.path(tmpdir, "scle.loom")
+)
+
+dropletutils_input_dir <- file.path(tmpdir, "tenx_input_dir")
+dir.create(dropletutils_input_dir)
+
+file.symlink(from = params$mtx_file, to = file.path(dropletutils_input_dir))
+file.symlink(from = params$barcodes_file, to = file.path(dropletutils_input_dir))
+file.symlink(from = params$genes_file, to = file.path(dropletutils_input_dir))
+
+## ---- task ---------------------------------------------------------------
+
+sce <- DropletUtils::read10xCounts(samples = dropletutils_input_dir, sample.names = params$sample_name, type = "mtx")
 
 ## ---- outputs ------------------------------------------------------------
+
+scle <- as(sce, "SingleCellLoomExperiment")
+export(scle, params$outfile)
 
 ## Provenance to the job log.
 message(paste(utils::capture.output(utils::sessionInfo()), collapse = "\n"))
